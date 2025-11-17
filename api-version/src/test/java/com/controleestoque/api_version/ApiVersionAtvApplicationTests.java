6b373bc6cd9b5080da7861a15523f6d2cd90a2c6
PEDRO MARINHO RODRIGUES CAMARGO CORRÊA <pedro.correa45@etec.sp.gov.br>
2025-11-17 10:06:56 -0300

    first commit

---
+++ api-version/src/test/java/com/controleestoque/api_version/ApiVersionAtvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controleestoque.api_version;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVersionAtvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
